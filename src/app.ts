fe879c8ba31da318ec7cc4d77d514c425be1d4dd
mehadishisir <mehadishisir@gmail.com>
2026-06-30 07:25:04 +0600

    Registering First User With Prisma And Register API

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import cookieParser from "cookie-parser";
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import config from "./config";
+import { prisma } from "./lib/prisma";
+import httpStatus from "http-status";
+import bcrypt from "bcryptjs";
 
 const app:Application =  express();
 app.use(cors({
@@ -14,7 +17,66 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+
+app.post("/api/users/register", async (req:Request,res:Response)=>{
+  const {email,password,name,profilePhoto} = req.body;
+  const isUserExist = await prisma.user.findUnique(
+    {
+      where : {email}
+  })
+  if(isUserExist){
+    return res.status(httpStatus.BAD_REQUEST).json({
+      message:"user with this email already exist"
+    })
+
+  }
+
+  const hashPassword = await bcrypt.hash(password,Number(config.bycrypt_salt_rounds))
+
+  // console.log(hashPassword)
+
+  const createUser = await prisma.user.create({
+    data :{
+      name,
+      email,
+      password : hashPassword,
+
+
+    }
+  })
+   await prisma.profile.create({
+    data : {
+      userId : createUser.id,
+      profilePhoto
+
+    }
+  })
+  const user = await prisma.user.findUnique({
+    where : {
+      id : createUser.id,
+      email:createUser.email
+    },
+    omit:{
+      password
+    }
+    ,include:{
+      profile:true
+    }
+  })
+  res.status(httpStatus.CREATED).json({
+
+    message : "User registered successfully",
+    data : user
+  })
+})
+
+app.get("/", async (req: Request, res: Response) => {
+  const user = await prisma.user.findMany();
+console.log("users:",user)
+  return res.status(httpStatus.OK).json({
+    success: true,
+    data: user,
+  });
 });
+
 export default app;
