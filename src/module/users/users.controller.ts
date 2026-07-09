518db41088ad9010d9394bbb25cd3893b3ac43e8
mehadishisir <mehadishisir@gmail.com>
2026-07-09 18:34:12 +0600

    explore cach async

--- src/module/users/users.controller.ts
+++ src/module/users/users.controller.ts
@@ -1,29 +1,42 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, RequestHandler, Response } from "express";
 import { prisma } from "../../lib/prisma";
 import httpStatus from "http-status";
 import bcrypt from "bcryptjs";
 import config from "../../config";
 import { userService } from "./users.service";
+import { catchAsync } from "../../utils/catchAsync";
+
+
 // regusteruser
-const registerUser = async (req:Request,res:Response)=>{
-try {
-      const payload = req.body;
- const user = await userService.registerUserIntoDB(payload)
-  res.status(httpStatus.CREATED).json({
-    success:true,
+// const registerUser = async (req:Request,res:Response)=>{
+// try {
+//       const payload = req.body;
+//  const user = await userService.registerUserIntoDB(payload)
+//   res.status(httpStatus.CREATED).json({
+//     success:true,
 
-    message : "User registered successfully",
-    data : user,
-  });
+//     message : "User registered successfully",
+//     data : user,
+//   });
 
-} catch (error) {
-    console.log(error)
-    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-        success : false,
-        message:"something went wrong"
-    })
-}
-}
+// } catch (error) {
+//     console.log(error)
+//     return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+//         success : false,
+//         message:"something went wrong"
+//     })
+// }
+// }
+
+const registerUser = catchAsync(async (req:Request,res:Response)=>{
+    const payload = req.body;
+    const user = await userService.registerUserIntoDB(payload) 
+     res.status(httpStatus.CREATED).json({
+    success: true,
+    message: "User registered successfully",
+    data: user,
+  }); 
+})
 export const userController ={
     registerUser
 }
