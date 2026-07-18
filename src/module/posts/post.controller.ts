d498759bedbe8a603a973d71bb2cdca50c0e7ebd
mehadishisir <mehadishisir@gmail.com>
2026-07-19 02:09:34 +0600

    create post and get all post api created successfully

--- src/module/posts/post.controller.ts
+++ src/module/posts/post.controller.ts
@@ -1,12 +1,31 @@
 import { NextFunction, Request, Response } from "express";
 import { catchAsync } from "../../utils/catchAsync";
+import { postService } from "./post.service";
+import { sendResponse } from "../../utils/sendResponse";
+import httpStatus from "http-status";
 
 const createPost = catchAsync(async(req:Request,res:Response,next:NextFunction)=>{
-    
+   const id = req.user?.id
+   const payload = req.body
+   const result = await postService.createPost(payload,id as string) ;
+
+   sendResponse(res,{
+    success:true,
+    statusCode:httpStatus.CREATED,
+    message : "Post Created SuccessFully",
+    data:result
+   })
 
 })
 const getAllPosts = catchAsync(async(req:Request,res:Response,next:NextFunction)=>{
-    
+    const result = await postService.getAllPost()
+
+    sendResponse(res,{
+        success:true,
+        statusCode:httpStatus.OK,
+        message:"get all posts seccessfully",
+        data:result
+    })
 
 })
 const getMyposts = catchAsync(async(req:Request,res:Response,next:NextFunction)=>{
--- src/module/posts/post.service.ts
+++ src/module/posts/post.service.ts
@@ -1,9 +1,33 @@
 import { NextFunction, Request } from "express";
+import { ICreatePost } from "./post.interface";
+import { prisma } from "../../lib/prisma";
 
-const createPost = async()=>{
-    return 
+const createPost = async(payload:ICreatePost ,userId:string)=>{
+    const result = await prisma.post.create(
+        {
+           data:{
+            ...payload,
+            authorId:userId
+           }
+        }
+    )
+    return result
 }
-
+ 
+const getAllPost = async()=>{
+    const result = await prisma.post.findMany({
+        include:{
+            author:{
+                omit:{
+                    password:true
+                }
+            },
+            comments:true
+        }
+    })
+    return result
+}
 export const postService ={
-    createPost
+    createPost,
+    getAllPost
 }
